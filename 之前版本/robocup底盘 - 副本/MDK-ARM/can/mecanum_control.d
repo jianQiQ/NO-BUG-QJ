63cfebbbb0739f4b37510a76907235ed81ab4270
jianQiQ <544675477@qq.com>
2025-10-06 20:30:33 +0800

    左右激光都完成了
    分别在stp.c,it.c，注意左右函数名不一致
    发送数据延时到了50ms

--- 之前版本/robocup底盘 - 副本/MDK-ARM/can/mecanum_control.d
+++
@@ -1,6 +0,0 @@
-can\mecanum_control.o: ..\application\mecanum_control.c
-can\mecanum_control.o: ..\application\mecanum_control.h
-can\mecanum_control.o: ..\application\struct_typedef.h
-can\mecanum_control.o: ..\application\CAN_receive.h
-can\mecanum_control.o: D:\kile5\ARM\ARMCC\Bin\..\include\math.h
-can\mecanum_control.o: D:\kile5\ARM\ARMCC\Bin\..\include\stdlib.h
